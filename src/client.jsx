7f570d65b6c60a428b2f9709b27efc7a15921824
bodie <bodo.braegger@pm.me>
2023-06-19 16:03:59 +0200

    move backend url to .env

--- src/client.jsx
+++ src/client.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'https://strapi.app.levell.ch/',
+  baseURL: process.env.REACT_APP_BACKEND_URL, // 'https://strapi.app.levell.ch/',
   headers: {
     "Content-type": "application/json",
   },
