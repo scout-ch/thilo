cdee4dc7f838a651d6a6eb8bd0fea85239649ab8
bodie <bodo.braegger@pm.me>
2023-08-27 23:41:55 +0200

    cleanup, remove unused files

--- craco.config.js
+++ craco.config.js
@@ -6,7 +6,7 @@ module.exports = {
         // Prefer 'sass' (dart-sass) over 'node-sass' if both packages are installed.
         implementation: require('sass'),
         // Workaround for this bug: https://github.com/webpack-contrib/sass-loader/issues/804
-        webpackImporter: false,
+        // webpackImporter: false,
       },
     },
   },
