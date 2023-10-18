1f8b286b20fdf3a1111845a35b900e81ab88225b
bodie <bodo.braegger@pm.me>
2023-10-18 17:28:06 +0200

    feat: Add low config PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import sass from 'sass'
 
 
@@ -7,7 +8,11 @@ export default defineConfig({
   build: {
     outDir: 'build'
   }, 
-  plugins: [react()],
+  base: '/thilo/',
+  plugins: [react(), 
+    VitePWA(
+      {registerType: 'autoUpdate'}
+  )],
   css: {
     preprocessorOptions: {
       scss: {
