a782cf6536815c0ecdd18236f19811c7b68979f3
James <jimmy.levell@bluewin.ch>
2023-10-18 15:41:58 +0200

    Fixed Env Window

--- src/client.jsx
+++ src/client.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const windowEnv = window.env.BACKEND_URL !== "${BACKEND_URL}" ? window.env.BACKEND_URL : ""
+
 export default axios.create({
   // read baseULR for dev from .env file, for production from window.env (set by webpack)
-  baseURL: process.env.REACT_APP_BACKEND_URL || window.env.BACKEND_URL || 'https://strapi.app.levell.ch/',
+  baseURL: process.env.REACT_APP_BACKEND_URL || windowEnv || 'https://strapi.app.levell.ch/',
   headers: {
     "Content-type": "application/json",
   },
