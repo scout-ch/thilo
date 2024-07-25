0551d2de8554485ca0bdbb64f8e1c50ca1a77d63
bodobraegger <braegger.bodo@gmail.com>
2024-07-25 13:36:45 +0200

    feat: single page fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,13 @@ import sass from 'sass'
 
 export default defineConfig({
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    rollupOptions: {
+      input: entryPoints(
+        "index.html",
+        "404.html",
+      )
+    }
   },
   base: process.env.REACT_APP_PUBLIC_URL,
   plugins: [react(),
@@ -21,3 +27,17 @@ export default defineConfig({
     },
   },
 });
+
+import { join, parse, resolve } from "path";
+
+function entryPoints(...paths) {
+  const entries = paths.map(parse).map(entry => {
+    const { dir, base, name, ext } = entry;
+    const key = join(dir, name);
+    const path = resolve(__dirname, dir, base);
+    return [key, path];
+  });
+  
+  const config = Object.fromEntries(entries);
+  return config;
+}
